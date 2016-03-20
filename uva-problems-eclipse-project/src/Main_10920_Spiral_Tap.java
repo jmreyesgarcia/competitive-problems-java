39bb3f6b63ad8e34176120f7f44e71aeaab27abe
jmreyesgarcia <jmreyesgarcia@gmail.com>
2016-03-20 00:22:51 -0500

    Reubicación y renombramiento de los ejercicios spiral tap, el que no funcionó inicialmente y el que finalmente funcionó

--- uva-problems-eclipse-project/src/Main_10920_Spiral_Tap.java
+++ uva-problems-eclipse-project/src/Main_10920_Spiral_Tap.java
@@ -5,7 +5,8 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 
-public class Main_10920_Spiral_TapV1 {
+public class Main_10920_Spiral_Tap {
+	/*
 	public static void print(int[][] spiralTap, BufferedWriter bw) throws IOException{
 		for (int k = 0; k < spiralTap.length; k++) {
 			for (int k2 = 0; k2 < spiralTap.length; k2++) {
@@ -16,7 +17,7 @@ public class Main_10920_Spiral_TapV1 {
 			System.out.println("");
 		}		
 		System.out.println("");
-	}
+	}*/
 	public static void main(String[] args) throws IOException{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -27,16 +28,37 @@ public class Main_10920_Spiral_TapV1 {
 			int s = Integer.parseInt(sp[0]);
 			long p = Long.parseLong(sp[1]);
 			int i;
-			int j = i = s-1;
-			long count = (long)s*(long)s;
+			int j;
+			//long count = (long)s*(long)s;
 			
-			boolean encontro = false;
-			int min = 0;
-			int max = s;
-			while(count>0 && !encontro){
+			boolean finded = false;
+			int min/* = 0*/;
+			int max/* = s*/;
+			
+			int rootSquare = (int)Math.sqrt(p);
+			//bw.write(" p="+p+", rootSquare="+rootSquare+"\n");
+			if(rootSquare%2==0){
+				rootSquare++;
+			}
+			//bw.write("*p="+p+", rootSquare="+rootSquare+"\n");
+			int count = rootSquare*rootSquare;
+			//bw.write("s="+s+", p="+p+", count="+count+"\n");
+			if(count<p){ //no creo que sea necesario
+				rootSquare+=2;
+				count = rootSquare*rootSquare;
+			}
+				
+			min = (s-rootSquare)/2;
+			//bw.write("min="+min+"\n");
+			max = s - min;
+			//bw.write("max="+max+"\n");
+			i = j = max - 1;
+
+			while(count>0 && !finded){
 				while(i>=min/*0 && spiralTap[i][j]==0*/){
+					//System.out.println("A, i="+i);
 					if(count==p){
-						encontro = true;
+						finded = true;
 						bw.write("Line = "+(i+1)+", column = "+(j+1)+".\n");
 					}
 					/*spiralTap[i][j] = */count--; 
@@ -46,8 +68,9 @@ public class Main_10920_Spiral_TapV1 {
 				i++; j--;
 				
 				while(j>=min/*0 && spiralTap[i][j]==0*/){
+					//System.out.println("B, j="+j);
 					if(count==p){
-						encontro = true;
+						finded = true;
 						bw.write("Line = "+(i+1)+", column = "+(j+1)+".\n");
 					}
 					/*spiralTap[i][j] = */count--; 
@@ -58,8 +81,9 @@ public class Main_10920_Spiral_TapV1 {
 				min++;
 
 				while(i<max/*s && spiralTap[i][j]==0*/){
+					//System.out.println("C, i="+i);
 					if(count==p){
-						encontro = true;
+						finded = true;
 						bw.write("Line = "+(i+1)+", column = "+(j+1)+".\n");
 					}
 					/*spiralTap[i][j] = */count--; 
@@ -70,15 +94,17 @@ public class Main_10920_Spiral_TapV1 {
 				max--;
 				
 				while(j<max/*s && spiralTap[i][j]==0*/){
+					//System.out.println("D, j="+j);
 					if(count==p){
-						encontro = true;
+						finded = true;
 						bw.write("Line = "+(i+1)+", column = "+(j+1)+".\n");
 					}
 					/*spiralTap[i][j] = */count--; 
 					j++;
 				}
 				
-				j--; i--;				
+				j--; i--;
+				//bw.flush();
 			}
 			//print(spiralTap,bw);
 		}
