33282740c396b526c98d4339577e3e6659f22256
jmreyes <oqdoqf37>
2017-09-04 18:10:36 -0500

    se arreglan problemas de compilacion y se hacen pruebas sobre los casos basicos del enunciado

--- uva-problems-eclipse-project/src/RPC_2017_09_Bolivia_I_SmallestGreater.java
+++ uva-problems-eclipse-project/src/RPC_2017_09_Bolivia_I_SmallestGreater.java
@@ -1,15 +1,15 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
+import java.io.*;
+
 public class RPC_2017_09_Bolivia_I_SmallestGreater {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamReader(System.out));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         String l = br.readLine();
         while (l != null) {
             String[] par = l.split(" ");
             long M = Long.parseLong(par[0]);
-            long K = Long.parseLong(par[1]);
+            int K = Integer.parseInt(par[1]);
             String Mb2 = Long.toString(M,2);
             int unosCons = 0;
             int unosDesp = 0;
@@ -25,6 +25,8 @@ public class RPC_2017_09_Bolivia_I_SmallestGreater {
                     cons = false;
                 }
             }
+            //bw.write("M="+M+", K="+K+"\n");
+            //bw.write("unosCons="+unosCons+", unosDesp="+unosDesp+"\n");
             String out = "";
             if(unosCons<K){
                 for(int i=0;i<K;i++){
@@ -35,7 +37,7 @@ public class RPC_2017_09_Bolivia_I_SmallestGreater {
                 }
                 long outL = Long.parseLong(out,2);
                 bw.write(outL+"\n");
-            }else if(unosCons==K && unosDesp==0){
+            }else if(unosDesp==0){
                 bw.write(M+"\n");
             }else{
                 for(int i=0;i<K;i++){
@@ -49,5 +51,7 @@ public class RPC_2017_09_Bolivia_I_SmallestGreater {
             }
             l = br.readLine();
         }
+        br.close();
+        bw.close();
     }
 }
